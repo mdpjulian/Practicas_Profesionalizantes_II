93fbbf16ca837c87661d7bfa703d29f06e87aa66
kunkkas <mdpjulian@gmail.com>
2021-11-11 16:52:06 -0300

    add api_fetch_js

---
+++ API_FETCH_JS/fetch.js
@@ -0,0 +1,16 @@
+const data = { service: 'TestService', action: 'read' };
+
+fetch('http://192.168.220.128/api/main.bin', {
+    method: 'POST', // or 'PUT'
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
